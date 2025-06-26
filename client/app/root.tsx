819fe38788875591e5b37a9fe093e521cca97667
Kaiyrkhan <kairolla222@gmail.com>
2025-06-26 23:43:09 +0500

    set site title

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -29,6 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <title>PROmentor</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
